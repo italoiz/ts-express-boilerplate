c7e9b26ff31b3bc745ac1f7f9ca2c46d85d2e12a
Italo Izaac <italoiz.dev@gmail.com>
2020-06-04 21:07:41 -0300

    feat: Add providers loader on app startup

--- src/shared/providers/index.ts
+++ src/shared/providers/index.ts
@@ -1,3 +1,30 @@
 import 'reflect-metadata';
+import glob from 'glob';
 
-import './JobProvider';
+import debug from '@utils/debug';
+
+const providers = [
+  'src/shared/providers/*/index.ts',
+  'src/modules/**/providers/*/index.ts',
+]
+  .map(pattern => glob.sync(pattern))
+  .reduce((acc, arr) => acc.concat(arr), []);
+
+const registerProviders = async (): Promise<void> => {
+  debug('providers', 'Registering providers');
+  debug('providers', 'Found %d provider(s) to register', providers.length);
+
+  if (!providers.length) return;
+
+  const modules = providers
+    .map(modulePath => modulePath.replace('src/', '@app/'))
+    .map(modulePath =>
+      import(modulePath).then(() => {
+        debug('providers', 'Provider "%s" registered', modulePath);
+      }),
+    );
+
+  await Promise.all(modules);
+};
+
+registerProviders();
